be921b35ad5f250d9b27c4ffe5f47de369e1cd79
Late-Smoke <3136906856@qq.com>
2025-01-15 20:20:21 +0800

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>你好home</div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        你好login
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
